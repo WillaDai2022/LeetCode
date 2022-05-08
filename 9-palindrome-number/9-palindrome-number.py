581d28b7c2df0a8e5d0f755e1cf778b6a18e1b81
WillaDai2022 <53583950+WillaDai2022@users.noreply.github.com>
2022-05-08 11:28:52 -0400

    Time: 67 ms (78.85%), Space: 13.9 MB (61.48%) - LeetHub

--- 9-palindrome-number/9-palindrome-number.py
+++ 9-palindrome-number/9-palindrome-number.py
@@ -1,20 +1,11 @@
-class Solution(object):
-    def isPalindrome(self, x):
-        """
-        :type x: int
-        :rtype: bool
-        """
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        s = str(x)
         
-        #x is negative or x ends with 0 but is not 0 (e.g. 10 100 1000...)
-        if x < 0 or (x % 10 == 0 and x != 0):
-            return False
-        
-        x_reverse = 0
-        while x > x_reverse:
-            #get the reversed x
-            x_reverse = x_reverse*10 + x%10
-            # remove the last digit of x each time
-            x //= 10
+        while s:
+            if s[0] == s[-1]:
+                s = s[1: len(s) - 1]
+            else:
+                return False
             
-        return x ==  x_reverse or x == x_reverse // 10
-        
+        return True
