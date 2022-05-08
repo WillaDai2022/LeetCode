4accf674cac0b9a3c96853bded3e309aa27f77d2
WillaDai2022 <53583950+WillaDai2022@users.noreply.github.com>
2022-05-08 16:30:27 -0400

    Time: 35 ms (87.49%), Space: 14 MB (12.29%) - LeetHub

--- 14-longest-common-prefix/14-longest-common-prefix.py
+++ 14-longest-common-prefix/14-longest-common-prefix.py
@@ -1,13 +1,10 @@
-class Solution(object):
-    def longestCommonPrefix(self, strs):
-        """
-        :type strs: List[str]
-        :rtype: str
-        """
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
         prefix = strs[0]
         
         for str in strs:
-            while str.find(prefix) != 0:
+            while not str.startswith(prefix):
                 prefix = prefix[:len(prefix) - 1]
+                
                 
         return prefix
