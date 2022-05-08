803fcc4ac55903c26fdd8c3174dbb5dc50568fff
WillaDai2022 <53583950+WillaDai2022@users.noreply.github.com>
2022-05-08 16:19:46 -0400

    Time: 43 ms (95.47%), Space: 13.9 MB (77.91%) - LeetHub

--- 13-roman-to-integer/13-roman-to-integer.py
+++ 13-roman-to-integer/13-roman-to-integer.py
@@ -1,33 +1,22 @@
-class Solution(object):
-    def romanToInt(self, s):
-        """
-        :type s: str
-        :rtype: int
-        """
-
-        #dictionary storing all the symbols
+class Solution:
+    def romanToInt(self, s: str) -> int:
         dict1 = {
-            "I":1,
-            "V":5,
-            "X":10,
-            "L":50,
-            "C":100,
-            "D":500,
-            "M":1000
-        }
-        # current val equals the smallest value
-        curr_val = 1
+        "I":1,
+        "V":5,
+        "X":10,
+        "L":50,
+        "C":100,
+        "D":500,
+        "M":1000}
+        
+        curr = 1
         res = 0
         
-        #loop numerals from end to start
         for char in s[::-1]:
-            #if value represent by the numeral greater than or equals to current value
-            if dict1[char] >= curr_val:
-                #add the value to result
+            if dict1[char] >= curr:
                 res += dict1[char]
-                #current value equals to the value represented by this numeral
-                curr_val = dict1[char]
             else:
-                #subtract from the result
                 res -= dict1[char]
+            curr =  dict1[char]
+                
         return res
