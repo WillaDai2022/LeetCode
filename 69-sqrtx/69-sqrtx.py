a00d2bb6072dcf6fb1a231a0064c7fbf495c8298
WillaDai2022 <53583950+WillaDai2022@users.noreply.github.com>
2022-05-09 11:29:48 -0400

    Time: 66 ms (35.84%), Space: 13.8 MB (96.47%) - LeetHub

--- 69-sqrtx/69-sqrtx.py
+++ 69-sqrtx/69-sqrtx.py
@@ -8,9 +8,9 @@ class Solution:
         
         while(l <= r):
             middle = (l + r) // 2
-            if middle*middle == x:
+            if middle == x // middle:
                 return middle
-            elif middle*middle > x:
+            elif middle > x // middle:
                 r = middle - 1
             else:
                 l = middle + 1 
