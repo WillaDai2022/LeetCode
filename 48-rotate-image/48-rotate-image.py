f0a533436efe25801a9dab013a4bb727902672e9
WillaDai2022 <53583950+WillaDai2022@users.noreply.github.com>
2022-06-03 12:47:02 -0400

    Time: 54 ms (37.36%), Space: 13.9 MB (28.94%) - LeetHub

--- 48-rotate-image/48-rotate-image.py
+++ 48-rotate-image/48-rotate-image.py
@@ -12,16 +12,15 @@ class Solution:
         
         #We change 4 points at once. If n is even, we should make n^2 / 4 = (n/2)(n/2) changes and if n is odd, we should make (n^2-1) / 4 = ((n-1)/2) ((n+1)/2) times because we dont move the center
         
-        for i in range(n//2):
-            for j in range((n+1) // 2):
-                temp = matrix[j][n -1 - i]
-                matrix[j][n -1 - i] = matrix[i][j]
+        for i in range(n // 2):
+            for j in range((n+1) //2):
+                temp = matrix[j][n - 1 - i]
+                matrix[j][n - 1 - i] = matrix[i][j]
                 matrix[i][j] = matrix[n - 1 - j][i]
-                matrix[n - 1 - j][i] = matrix[n-1 -i][n - 1 - j]
-                matrix[n-1 -i][n - 1 - j] = temp
-                
+                matrix[n - 1 - j][i] = matrix[n - 1 - i][n - 1 - j]
+                matrix[n - 1 - i][n - 1 - j] = temp
             
-            
+        
         
         
         
