d330fc6546776470031903a1ab6faad7f648c2f1
WillaDai2022 <53583950+WillaDai2022@users.noreply.github.com>
2022-06-06 23:12:18 -0400

    Time: 64 ms (69.91%), Space: 13.9 MB (73.83%) - LeetHub

--- 80-remove-duplicates-from-sorted-array-ii/80-remove-duplicates-from-sorted-array-ii.py
+++ 80-remove-duplicates-from-sorted-array-ii/80-remove-duplicates-from-sorted-array-ii.py
@@ -1,13 +1,11 @@
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
-       
-        #p指向第一个无效位置
-        p = 2
+        left = 2
+        
+        for i in range(2,len(nums)):
+            if nums[i] != nums[left-2]:
+                nums[left] = nums[i]
+                left += 1
                 
-        for i in range(2, len(nums)):
-            if nums[i] != nums[p-2]:
-                nums[p] = nums[i]
-                p += 1
+        return left
                 
-        return p
-                    
