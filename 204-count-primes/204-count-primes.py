3d9412bee73eed66f38245c610664c44c033fd2a
WillaDai2022 <53583950+WillaDai2022@users.noreply.github.com>
2022-06-07 20:13:21 -0400

    Time: 5433 ms (40.50%), Space: 52.8 MB (62.52%) - LeetHub

--- 204-count-primes/204-count-primes.py
+++ 204-count-primes/204-count-primes.py
@@ -6,8 +6,11 @@ class Solution:
         primes = [True]*n
         scope = int(math.sqrt(n))
         
+        # 从 2 开始枚举到 sqrt(n)。
         for i in range(2, scope + 1):
+            # 如果当前是素数
             if primes[i]:
+                # 就把从 i*i 开始，i 的所有倍数都设置为 false
                 for j in range(i*i, n, i):
                     primes[j] = False
                     
