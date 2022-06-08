bfd3738e80ed5c33734ddfcfe3328c9e746a8250
WillaDai2022 <53583950+WillaDai2022@users.noreply.github.com>
2022-06-08 15:23:46 -0400

    Time: 6080 ms (32.89%), Space: 52.8 MB (62.52%) - LeetHub

--- 204-count-primes/204-count-primes.py
+++ 204-count-primes/204-count-primes.py
@@ -1,22 +1,16 @@
 class Solution:
     def countPrimes(self, n: int) -> int:
-        
+        # 埃氏筛
+        prime = [True]*n
         count = 0
         
-        primes = [True]*n
-        scope = int(math.sqrt(n))
-        
-        # 从 2 开始枚举到 sqrt(n)。
-        for i in range(2, scope + 1):
-            # 如果当前是素数
-            if primes[i]:
-                # 就把从 i*i 开始，i 的所有倍数都设置为 false
+        for i in range(2, int(math.sqrt(n))+1):
+            if prime[i]:
                 for j in range(i*i, n, i):
-                    primes[j] = False
-                    
-        for i in range(2, n):
-            if primes[i]:
+                    prime[j] = False
+        
+        for i in range(2,n):
+            if prime[i]:
                 count += 1
                 
         return count
-                    
