8237f618d8e7274bac013bbaf6d0151d33dde15f
WillaDai2022 <53583950+WillaDai2022@users.noreply.github.com>
2022-06-08 15:12:02 -0400

    Time: 484 ms (34.26%), Space: 16.3 MB (90.68%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.py
+++ 200-number-of-islands/200-number-of-islands.py
@@ -4,23 +4,30 @@ class Solution:
         rows = len(grid)
         cols = len(grid[0])
         count = 0
+       
         
-        def dfs(r, c):
-            if (not 0 <= r < rows or 
-                not 0 <= c < cols or 
-                grid[r][c] == '0'):
-                return
+        def bfs(r, c):
+            queue = collections.deque()
+            queue.append((r,c))
+            directions = [(1,0), (0,1), (-1,0), (0,-1)]
             
-            grid[r][c] = '0'
-            dfs(r-1, c)
-            dfs(r+1, c)
-            dfs(r, c-1)
-            dfs(r, c+1)
+            while queue:
+                row, col = queue.popleft()
+                for d1, d2 in directions:
+                    r = d1 + row
+                    c = d2 + col
+                    if (0 <= r < rows and 
+                        0 <= c < cols and 
+                        grid[r][c] == "1"):
+                        queue.append((r, c))
+                        grid[r][c] = "0"
+                
         
         for r in range(rows):
             for c in range(cols):
                 if grid[r][c] == "1":
-                    dfs(r, c)
+                    grid[r][c] = "0"
+                    bfs(r, c)
                     count += 1
                     
         return count
