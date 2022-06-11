a3abda7ee8b4b77136532e4f97b5150689f058b5
WillaDai2022 <53583950+WillaDai2022@users.noreply.github.com>
2022-06-10 20:38:03 -0400

    Time: 2639 ms (5.06%), Space: 17.7 MB (58.01%) - LeetHub

--- 220-contains-duplicate-iii/220-contains-duplicate-iii.py
+++ 220-contains-duplicate-iii/220-contains-duplicate-iii.py
@@ -7,10 +7,13 @@ class Solution:
         for idx, n in enumerate(nums):
             if idx > k:
                 window.remove(nums[idx -1 -k])
-                
+            
+            #如果n-t比window[-1]大，那么n+t肯定也比window[-1]大，则pos1 = pos2 = len(window) - 1
+            #如果n+t比window[0]小，那么n-t肯定也比window[0]小，则pos1 = pos2 = 0
+            #其余情况说明要么n-t比最小值大，要么n+t比最大值小或者同时
             pos1 = bisect_left(window, n-t)
             pos2 = bisect_right(window, n+t)
-            
+        
             if pos1 != pos2:
                 return True
               
