22e4b21635ac937fc3b10171f89542770148967c
WillaDai2022 <53583950+WillaDai2022@users.noreply.github.com>
2022-06-12 16:37:52 -0400

    Time: 56 ms (73.81%), Space: 14.3 MB (37.41%) - LeetHub

--- 281-zigzag-iterator/281-zigzag-iterator.py
+++ 281-zigzag-iterator/281-zigzag-iterator.py
@@ -3,8 +3,8 @@ from collections import deque
 class ZigzagIterator:
     def __init__(self, v1: List[int], v2: List[int]):
         #The function reversed() returns an iterator, not an actual list. You cannot directly get the len() of an iterator
-        self.v1 = list(reversed(v1))
-        self.v2 = list(reversed(v2))
+        self.v1 = list(reversed(v1)) #or v1[::-1]
+        self.v2 = list(reversed(v2)) # or v2[::-1]
         self.v1_flag = True
 
     def next(self) -> int:
