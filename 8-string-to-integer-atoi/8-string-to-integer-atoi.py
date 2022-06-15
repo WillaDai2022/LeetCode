2b38f0488d51e4f20d969f6d8b03f32812d83aff
WillaDai2022 <53583950+WillaDai2022@users.noreply.github.com>
2022-06-15 10:27:23 -0400

    Time: 63 ms (23.79%), Space: 14 MB (29.51%) - LeetHub

--- 8-string-to-integer-atoi/8-string-to-integer-atoi.py
+++ 8-string-to-integer-atoi/8-string-to-integer-atoi.py
@@ -1,20 +1,10 @@
 class Solution:
     def myAtoi(self, s: str) -> int:
         s = s.strip()
-        res = ""
+       
+        res_list = re.findall(r"^[\+\-]?\d+", s) #r"^[-|+]?\d+"
         
-        for c in s:
-            if not res and (c in ["-", "+"] or c.isdigit()):
-                res += c
-            elif c.isdigit():
-                res += c
-            else:
-                break
-                
-        if res and res not in ["-", "+"]:
-            if int(res)>0:
-                return min(2**31 -1, int(res))
-            else:
-                return max(-2**31, int(res))
-        return 0
+        #findall 返回一个列表,*用于unpack
+        num = int(*res_list)
         
+        return min(max(num, -2**31), 2**31-1)
