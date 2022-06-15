3848d5e0b29df839f37f9d5e0d5436169150bfae
WillaDai2022 <53583950+WillaDai2022@users.noreply.github.com>
2022-06-15 18:30:46 -0400

    Time: 142 ms (28.51%), Space: 15.4 MB (49.30%) - LeetHub

--- 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.py
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.py
@@ -1,45 +1,39 @@
 class Solution:
     def searchRange(self, nums: List[int], target: int) -> List[int]:
-        left = self.findStart(nums, target)
-        right = self.findEnd(nums, target)
-        
+        left = self.find_start(nums, target)
+        right = self.find_end(nums, target)
         return [left, right]
-    
-    
-    def findStart(self,nums,target):
-        
+       
+    def find_start(self,nums, target):
+        left, right = 0, len(nums)-1
         index = -1
-        left, right = 0, len(nums) - 1
-        
-        while left <= right:
-            middle = (left + right) // 2
 
-            if nums[middle] == target:
-                    index = middle
-
-            if nums[middle] >= target:
-                right = middle - 1
-            else:
-                left = middle + 1
-            
-        return index
-    
-    def findEnd(self, nums, target):
-        
-        index= -1
-        left, right = 0, len(nums) -1
-        
         while left <= right:
-            middle = (left + right) // 2 
-            
+            middle = (left + right)//2
             if nums[middle] == target:
                 index = middle
                 
+            if nums[middle] >= target:
+                right = middle -1
+            else:
+                left = middle + 1
+        return index
+
+    def find_end(self,nums, target):
+        left, right = 0, len(nums)-1
+        index = -1
+
+        while left <= right:
+            middle = (left + right)//2
+            if nums[middle] == target:
+                index = middle
+
             if nums[middle] <= target:
                 left = middle + 1
             else:
                 right = middle - 1
-                
+
         return index
-            
-            
+
+
+       
