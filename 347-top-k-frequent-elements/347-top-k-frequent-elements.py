063737acb869f601413854c427eb2f46736299df
WillaDai2022 <53583950+WillaDai2022@users.noreply.github.com>
2022-06-23 16:40:44 -0400

    Time: 115 ms (83.56%), Space: 18.7 MB (70.14%) - LeetHub

---
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.py
@@ -0,0 +1,21 @@
+import heapq
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        dic = {}
+        
+        #calculate the frequency of each number
+        for num in nums:
+            dic[num] = dic.get(num,0) + 1
+            
+        pri_queue = []
+        
+        for num, freq in dic.items():
+            heapq.heappush(pri_queue, (freq, num))
+            if len(pri_queue) > k:
+                heapq.heappop(pri_queue)
+                
+        res = []
+        for i in range(len(pri_queue)-1, -1, -1):
+            res.append(pri_queue.pop()[1])
+            
+        return res
