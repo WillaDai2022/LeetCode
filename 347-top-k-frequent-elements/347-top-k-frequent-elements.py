cc11a87f14aec266bf4af96cee2694f466460c45
WillaDai2022 <53583950+WillaDai2022@users.noreply.github.com>
2022-06-23 17:02:06 -0400

    Time: 196 ms (20.46%), Space: 19.7 MB (6.14%) - LeetHub

--- 347-top-k-frequent-elements/347-top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.py
@@ -1,23 +1,25 @@
 import heapq
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        
+        #桶排序
+        
         dic = {}
         
         #calculate the frequency of each number
         for num in nums:
             dic[num] = dic.get(num,0) + 1
-            
-        pri_queue = []
+        
+        #index stands for the frequency and the inner list stores the nums 
+        bucket = [[] for _ in range(len(nums)+1)]
         
         for num, freq in dic.items():
-            heapq.heappush(pri_queue, (freq, num))
-            if len(pri_queue) > k:
-                heapq.heappop(pri_queue)
+            bucket[freq].append(num)
+        
+        res = []
+        for i in range(len(bucket)-1, 0, -1):
+            for j in bucket[i]:
+                res.append(j)
+                if len(res) == k:
+                    return res
                 
-    
-        #找出前K个高频元素，因为小顶堆先弹出的是最小的，所以倒序来输出到数组
-        res = [0] * k
-        for i in range(k-1, -1, -1):
-            res[i] = heapq.heappop(pri_queue)[1]
-            
-        return res
