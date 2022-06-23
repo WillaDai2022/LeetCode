32f1e33bd4f52801a9acc042d7bb5f4a05734b91
WillaDai2022 <53583950+WillaDai2022@users.noreply.github.com>
2022-06-23 16:42:25 -0400

    Time: 148 ms (54.09%), Space: 18.7 MB (70.14%) - LeetHub

--- 347-top-k-frequent-elements/347-top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.py
@@ -14,9 +14,10 @@ class Solution:
             if len(pri_queue) > k:
                 heapq.heappop(pri_queue)
                 
-        res = []
-        #in any order
-        for i in range(len(pri_queue)-1, -1, -1):
-            res.append(pri_queue.pop()[1])
+    
+        #找出前K个高频元素，因为小顶堆先弹出的是最小的，所以倒序来输出到数组
+        res = [0] * k
+        for i in range(k-1, -1, -1):
+            res[i] = heapq.heappop(pri_queue)[1]
             
         return res
