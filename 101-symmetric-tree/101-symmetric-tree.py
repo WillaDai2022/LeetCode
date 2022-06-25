f8f104565d87b3696585923ace9d36ce694bc7f9
WillaDai2022 <53583950+WillaDai2022@users.noreply.github.com>
2022-06-25 00:36:29 -0400

    Time: 38 ms (85.72%), Space: 13.9 MB (93.71%) - LeetHub

--- 101-symmetric-tree/101-symmetric-tree.py
+++ 101-symmetric-tree/101-symmetric-tree.py
@@ -6,23 +6,28 @@
 #         self.right = right
 class Solution:
     def isSymmetric(self, root: Optional[TreeNode]) -> bool:
+        
         if not root:
             return True
         
-        def helper(node1, node2):
-            if not node1 and not node2:
-                return True
-            
-            if not node1 or not node2:
-                return False
-            
-            if node1.val != node2.val:
-                return False
-            
-            outside = helper(node1.left, node2.right)
-            inside =  helper(node1.right, node2.left)
-            
-            is_same = outside and inside
-            return is_same
+        from collections import deque
         
-        return helper(root.left, root.right)
+        queue = deque([root.left, root.right])
+        
+        while queue:
+            left = queue.popleft()
+            right = queue.popleft()
+            
+            if not left and not right:
+                continue
+            if not left or not right:
+                return False
+            if left.val != right.val:
+                return False
+            
+            queue.append(left.left)
+            queue.append(right.right)
+            queue.append(left.right)
+            queue.append(right.left)
+            
+        return True
