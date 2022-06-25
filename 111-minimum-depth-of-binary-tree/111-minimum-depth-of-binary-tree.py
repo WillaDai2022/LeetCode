ffbda68e5a3941efddc2d1dc3da2562a21930552
WillaDai2022 <53583950+WillaDai2022@users.noreply.github.com>
2022-06-24 21:02:53 -0400

    Time: 850 ms (39.72%), Space: 54.6 MB (42.72%) - LeetHub

--- 111-minimum-depth-of-binary-tree/111-minimum-depth-of-binary-tree.py
+++ 111-minimum-depth-of-binary-tree/111-minimum-depth-of-binary-tree.py
@@ -9,25 +9,10 @@ class Solution:
         if not root:
             return 0
         
-        queue = []
-        queue.append(root)
-        depth = 0
+        if not root.left or not root.right:
+            return 1 + max(self.minDepth(root.left), self.minDepth(root.right))
         
-        while queue:
-            size = len(queue)
-            depth += 1
-           
-            while size:
-                curr = queue.pop(0)
-                size -= 1
-                if not curr.left and not curr.right:
-                    return depth
-                if curr.left:
-                    queue.append(curr.left)
-                if curr.right:
-                    queue.append(curr.right)
-                    
-            
+        return 1 + min(self.minDepth(root.left), self.minDepth(root.right))
             
                     
  
