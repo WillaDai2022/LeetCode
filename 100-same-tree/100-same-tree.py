155227840f54dbd9192bd1d919ec626c613becd1
WillaDai2022 <53583950+WillaDai2022@users.noreply.github.com>
2022-07-04 09:57:41 -0400

    Time: 38 ms (75.52%), Space: 14 MB (29.42%) - LeetHub

--- 100-same-tree/100-same-tree.py
+++ 100-same-tree/100-same-tree.py
@@ -7,29 +7,20 @@
 class Solution:
     def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
       
-        if not p and not q:
-            return True
         
-        from collections import deque
-        
-        queue = deque([p,q])
-    
-        
-        while queue:
-            node1 = queue.popleft()
-            node2 = queue.popleft()
-            
+        def helper(node1, node2):
             if not node1 and not node2:
-                continue
+                return True
             
             if not node1 or not node2:
                 return False
+            
             if node1.val != node2.val:
                 return False
             
-            queue.append(node1.left)
-            queue.append(node2.left)
-            queue.append(node1.right)
-            queue.append(node2.right)
-            
-        return True
+            left = helper(node1.left, node2.left)
+            right = helper(node1.right, node2.right)
+            return left and right
+        
+        
+        return helper(p, q)
