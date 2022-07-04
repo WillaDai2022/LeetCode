273288d2b9f70987e4e7ff45f098438628ba7d59
WillaDai2022 <53583950+WillaDai2022@users.noreply.github.com>
2022-07-04 18:09:27 -0400

    Time: 59 ms (61.58%), Space: 15.2 MB (15.72%) - LeetHub

--- 112-path-sum/112-path-sum.py
+++ 112-path-sum/112-path-sum.py
@@ -4,12 +4,29 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+
+from collections import deque
 class Solution:
     def hasPathSum(self, root: Optional[TreeNode], targetSum: int) -> bool:
         if not root:
             return False
         
-        if not root.left and not root.right and targetSum - root.val == 0:
-            return True
+        nodes = deque([root])
+        sums = deque([targetSum])
         
-        return self.hasPathSum(root.left, targetSum-root.val) or self.hasPathSum(root.right, targetSum-root.val)
+        while nodes:
+            curr_node = nodes.pop()
+            curr_sum = sums.pop()
+            
+            if not curr_node.left and not curr_node.right and curr_sum - curr_node.val == 0:
+                return True
+            
+            if curr_node.right:
+                nodes.append(curr_node.right)
+                sums.append(curr_sum - curr_node.val)
+                
+            if curr_node.left:
+                nodes.append(curr_node.left)
+                sums.append(curr_sum - curr_node.val)
+                
+        return False
