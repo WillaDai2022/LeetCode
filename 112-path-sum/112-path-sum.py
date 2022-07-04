f2539c27eeaa08cf6cf55eaef8de8f8b2ccaa2e6
WillaDai2022 <53583950+WillaDai2022@users.noreply.github.com>
2022-07-04 16:24:57 -0400

    Time: 99 ms (5.21%), Space: 15 MB (92.88%) - LeetHub

--- 112-path-sum/112-path-sum.py
+++ 112-path-sum/112-path-sum.py
@@ -9,24 +9,7 @@ class Solution:
         if not root:
             return False
         
-        nodeStack = [root]
-        sumStack =[targetSum - root.val]
+        if not root.left and not root.right and targetSum - root.val == 0:
+            return True
         
-        while nodeStack:
-            curr = nodeStack.pop()
-            sum = sumStack.pop()
-            
-            if not curr.left and not curr.right and sum == 0:
-                return True
-            
-            if curr.left:
-                nodeStack.append(curr.left)
-                sumStack.append(sum - curr.left.val)
-                
-            if curr.right:
-                nodeStack.append(curr.right)
-                sumStack.append(sum - curr.right.val)
-                
-        return False
-            
-        
+        return self.hasPathSum(root.left, targetSum-root.val) or self.hasPathSum(root.right, targetSum-root.val)
