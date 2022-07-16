0cead9d7b0e1dbfbcc276b0f6df00051a2479672
WillaDai2022 <53583950+WillaDai2022@users.noreply.github.com>
2022-07-16 00:46:56 -0400

    Time: 56 ms (53.90%), Space: 13.8 MB (85.55%) - LeetHub

--- 88-merge-sorted-array/88-merge-sorted-array.py
+++ 88-merge-sorted-array/88-merge-sorted-array.py
@@ -3,24 +3,22 @@ class Solution:
         """
         Do not return anything, modify nums1 in-place instead.
         """
-        i = len(nums1) - 1
         
-        p1 = m - 1
-        p2 = n - 1
+        end = m + n - 1
+        idx1 = m - 1
+        idx2 = n - 1
         
-        while p1 >= 0 and p2 >=0:
-            if nums1[p1] > nums2[p2]:
-                nums1[i] = nums1[p1]
-                p1 -= 1
+        while idx1 >= 0 and idx2 >= 0:
+            if nums1[idx1] > nums2[idx2]:
+                nums1[end] = nums1[idx1]
+                idx1 -= 1
             else:
-                nums1[i] = nums2[p2]
-                p2 -= 1
-            i -= 1
-            
-        while p2 >= 0:
-            nums1[i] = nums2[p2]
-            p2 -= 1
-            i -= 1
-
-
-            
+                nums1[end] = nums2[idx2]
+                idx2 -= 1
+                
+            end -= 1
+                
+        while idx2 >= 0:
+            nums1[end] = nums2[idx2]
+            idx2 -= 1
+            end -= 1
